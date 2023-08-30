369a85bd16ec0d12028d1d3cb7278440b5e80c50
Roy de Kleijn <dekleijn.roy@gmail.com>
2023-08-30 09:09:20 +0200

    pages

--- sprint5/API/app/Http/Kernel.php
+++ sprint5/API/app/Http/Kernel.php
@@ -21,6 +21,7 @@ class Kernel extends HttpKernel
         \Illuminate\Http\Middleware\HandleCors::class,
         \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
+        \App\Http\Middleware\PaginateMiddleware::class
     ];
 
     protected $routeMiddleware = [
