b60560793673fca042787a566d427d9f0037307b
Roy de Kleijn <dekleijn.roy@gmail.com>
2023-10-05 15:03:44 +0200

    2 little fixes

--- sprint5/API/app/Http/Controllers/Controller.php
+++ sprint5/API/app/Http/Controllers/Controller.php
@@ -54,7 +54,7 @@ class Controller extends BaseController
 
     protected function preferredFormat($data, $status = 200, array $headers = [], $xmlRoot = 'response')
     {
-        if (strcmp(app('request')->headers->get('Accept'), 'xml') == 0) {
+        if (str_contains(app('request')->headers->get('Accept'), 'xml')) {
             return $this->makeXML($data, $status, array_merge($headers, ['Content-Type' => app('request')->headers->get('Accept')]), $xmlRoot);
         } else {
             return response()->json($data, $status,
