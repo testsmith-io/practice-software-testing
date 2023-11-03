d64688a657829ad599a0fff1ad9d154831332372
Roy de Kleijn <dekleijn.roy@gmail.com>
2023-11-03 13:36:09 +0100

    improved deployment

--- deploy.php
+++ deploy.php
@@ -34,9 +34,7 @@ add('shared_dirs', []);
 // Writable dirs by web server
 add('writable_dirs', []);
 
-
 // Hosts
-
 host('production')
     ->hostname('54.229.94.142')
     ->user('ubuntu')
@@ -45,49 +43,59 @@ host('production')
     ->set('deploy_path', '/var/www/api.practicesoftwaretesting.com');
 
 // Tasks
-task('upload', function () {
-    $apps = [
-//gf
-        ['source'=> 'sprint5', 'web_destination' => '', 'api_destination' => ''],
-        ['source'=> 'sprint4', 'web_destination' => 'v4.', 'api_destination' => '-v4'],
-        ['source'=> 'sprint3', 'web_destination' => 'v3.', 'api_destination' => '-v3'],
-        ['source'=> 'sprint2', 'web_destination' => 'v2.', 'api_destination' => '-v2'],
-        ['source'=> 'sprint1', 'web_destination' => 'v1.', 'api_destination' => '-v1'],
-        ['source'=> 'sprint5-with-bugs', 'web_destination' => 'with-bugs.', 'api_destination' => '-with-bugs']
-    ];
+task('upload-sprint1', function () {
     $deployPath = get('deploy_path');
-
-    foreach ($apps as $app)
-    {
-        deploy($app);
-    }
+    deploy('sprint1', 'v1.', '-v1');
 });
 
-function deploy($app) {
-    echo $app;
+task('upload-sprint2', function () {
+    $deployPath = get('deploy_path');
+    deploy('sprint2', 'v2.', '-v2');
+});
 
-    runLocally("cd {$app['source']}/API/ && composer update --no-dev --prefer-dist --optimize-autoloader");
-    run("cd /var/www/ && mkdir -p api{$app['api_destination']}.practicesoftwaretesting.com_tmp");
-    upload(__DIR__ . "/{$app['source']}/API/", "/var/www/api{$app['api_destination']}.practicesoftwaretesting.com_tmp");
-    run("sudo mv /var/www/api{$app['api_destination']}.practicesoftwaretesting.com /var/www/api{$app['api_destination']}.practicesoftwaretesting.com_bak");
-    run("sudo mv /var/www/api{$app['api_destination']}.practicesoftwaretesting.com_tmp /var/www/api{$app['api_destination']}.practicesoftwaretesting.com");
-    run("sudo rm -f -R /var/www/api{$app['api_destination']}.practicesoftwaretesting.com_tmp");
-    run("sudo rm -f -R /var/www/api{$app['api_destination']}.practicesoftwaretesting.com_bak");
-    run("curl https://api{$app['api_destination']}.practicesoftwaretesting.com");
-    run("/usr/bin/php /var/www/api{$app['api_destination']}.practicesoftwaretesting.com/artisan migrate:fresh --force -vvvv");
-    run("/usr/bin/php /var/www/api{$app['api_destination']}.practicesoftwaretesting.com/artisan db:seed --force -vvvv");
-    run("/usr/bin/php /var/www/api{$app['api_destination']}.practicesoftwaretesting.com/artisan route:cache");
-    run("/usr/bin/php /var/www/api{$app['api_destination']}.practicesoftwaretesting.com/artisan config:cache");
+task('upload-sprint3', function () {
+    $deployPath = get('deploy_path');
+    deploy('sprint3', 'v3.', '-v3');
+});
+
+task('upload-sprint4', function () {
+    $deployPath = get('deploy_path');
+    deploy('sprint4', 'v4.', '-v4');
+});
+
+task('upload-sprint5', function () {
+    $deployPath = get('deploy_path');
+    deploy('sprint5', '', '');
+});
+
+task('upload-sprint5-with-bugs', function () {
+    $deployPath = get('deploy_path');
+    deploy('sprint5-with-bugs', 'with-bugs.', '-with-bugs');
+});
+
+function deploy($source, $webDestination, $apiDestination) {
+    runLocally("cd {$source}/API/ && composer update --no-dev --prefer-dist --optimize-autoloader");
+    run("cd /var/www/ && mkdir -p api{$apiDestination}.practicesoftwaretesting.com_tmp");
+    upload(__DIR__ . "/{$source}/API/", "/var/www/api{$apiDestination}.practicesoftwaretesting.com_tmp");
+    run("sudo mv /var/www/api{$apiDestination}.practicesoftwaretesting.com /var/www/api{$apiDestination}.practicesoftwaretesting.com_bak");
+    run("sudo mv /var/www/api{$apiDestination}.practicesoftwaretesting.com_tmp /var/www/api{$apiDestination}.practicesoftwaretesting.com");
+    run("sudo rm -f -R /var/www/api{$apiDestination}.practicesoftwaretesting.com_tmp");
+    run("sudo rm -f -R /var/www/api{$apiDestination}.practicesoftwaretesting.com_bak");
+    run("curl https://api{$apiDestination}.practicesoftwaretesting.com");
+    run("/usr/bin/php /var/www/api{$apiDestination}.practicesoftwaretesting.com/artisan migrate:fresh --force -vvvv");
+    run("/usr/bin/php /var/www/api{$apiDestination}.practicesoftwaretesting.com/artisan db:seed --force -vvvv");
+    run("/usr/bin/php /var/www/api{$apiDestination}.practicesoftwaretesting.com/artisan route:cache");
+    run("/usr/bin/php /var/www/api{$apiDestination}.practicesoftwaretesting.com/artisan config:cache");
 //        run("/usr/bin/php /var/www/api{$app['api_destination']}.practicesoftwaretesting.com/artisan vendor:publish --provider \"L5Swagger\L5SwaggerServiceProvider\"");
-    run("/usr/bin/php /var/www/api{$app['api_destination']}.practicesoftwaretesting.com/artisan l5-swagger:generate");
-    run("sudo chmod -R 777 /var/www/api{$app['api_destination']}.practicesoftwaretesting.com/storage");
-    run("sudo chown -R www-data:www-data /var/www/api{$app['api_destination']}.practicesoftwaretesting.com/storage/framework");
+    run("/usr/bin/php /var/www/api{$apiDestination}.practicesoftwaretesting.com/artisan l5-swagger:generate");
+    run("sudo chmod -R 777 /var/www/api{$apiDestination}.practicesoftwaretesting.com/storage");
+    run("sudo chown -R www-data:www-data /var/www/api{$apiDestination}.practicesoftwaretesting.com/storage/framework");
 
-    runLocally("cd {$app['source']}/UI/ && npm install --legacy-peer-deps && npm run build");
-    run("cd /var/www/ && mkdir -p {$app['web_destination']}practicesoftwaretesting.com_tmp/public_html");
-    upload(__DIR__ . "/{$app['source']}/UI/dist/toolshop/", "/var/www/{$app['web_destination']}practicesoftwaretesting.com_tmp/public_html");
-    run("sudo mv /var/www/{$app['web_destination']}practicesoftwaretesting.com /var/www/{$app['web_destination']}practicesoftwaretesting.com_bak");
-    run("sudo mv /var/www/{$app['web_destination']}practicesoftwaretesting.com_tmp /var/www/{$app['web_destination']}practicesoftwaretesting.com");
-    run("sudo rm -f -R /var/www/{$app['web_destination']}practicesoftwaretesting.com_tmp");
-    run("sudo rm -f -R /var/www/{$app['web_destination']}practicesoftwaretesting.com_bak");
+    runLocally("cd {$source}/UI/ && npm install --legacy-peer-deps && npm run build");
+    run("cd /var/www/ && mkdir -p {$webDestination}practicesoftwaretesting.com_tmp/public_html");
+    upload(__DIR__ . "/{$source}/UI/dist/toolshop/", "/var/www/{$webDestination}practicesoftwaretesting.com_tmp/public_html");
+    run("sudo mv /var/www/{$webDestination}practicesoftwaretesting.com /var/www/{$webDestination}practicesoftwaretesting.com_bak");
+    run("sudo mv /var/www/{$webDestination}practicesoftwaretesting.com_tmp /var/www/{$webDestination}practicesoftwaretesting.com");
+    run("sudo rm -f -R /var/www/{$webDestination}practicesoftwaretesting.com_tmp");
+    run("sudo rm -f -R /var/www/{$webDestination}practicesoftwaretesting.com_bak");
 }
