7cf88545461eaf690843d38351f43de45de4b3e8
Roy de Kleijn <dekleijn.roy@gmail.com>
2024-05-12 09:10:59 +0200

    added Google GTM

--- sprint5/UI/src/app/_services/ga.service.ts
+++ sprint5/UI/src/app/_services/ga.service.ts
@@ -9,6 +9,8 @@ export class GaService {
   constructor() {
     if (environment.gaCode) {
       this.injectGaScript(environment.gaCode);
+      this.injectGtmScript(environment.gtmCode);
+      this.injectGtmNoScript(environment.gtmCode);
     }
   }
 
@@ -26,5 +28,27 @@ export class GaService {
       gtag('config', '${gaCode}', {'page_path': location.pathname + location.hash});
     `;
     document.head.appendChild(scriptText);
+  }
+
+
+  private injectGtmScript(gtmId: string): void {
+    const script = document.createElement('script');
+    script.innerHTML = `
+      (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+      new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+      j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+      'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+      })(window,document,'script','dataLayer','${gtmId}');
+    `;
+    document.head.appendChild(script);
+  }
+
+  private injectGtmNoScript(gtmId: string): void {
+    const noscript = document.createElement('noscript');
+    noscript.innerHTML = `
+      <iframe src="https://www.googletagmanager.com/ns.html?id=${gtmId}"
+      height="0" width="0" style="display:none;visibility:hidden"></iframe>
+    `;
+    document.body.insertBefore(noscript, document.body.firstChild);
   }
 }
