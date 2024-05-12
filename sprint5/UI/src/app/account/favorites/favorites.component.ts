f7a30e8b958f3291b2d17646d3fddc19450b9e0c
Roy de Kleijn <dekleijn.roy@gmail.com>
2024-05-12 09:25:40 +0200

    Revert "Disabled hash in url"
    This reverts commit dc9db2c696454692f829fa0585aa0faf95c6cdce.

--- sprint5/UI/src/app/account/favorites/favorites.component.ts
+++ sprint5/UI/src/app/account/favorites/favorites.component.ts
@@ -35,7 +35,7 @@ export class FavoritesComponent implements OnInit {
       }, (error) => {
         if (error.status === 401 || error.status === 403) {
           window.localStorage.removeItem('TOKEN_KEY');
-          window.location.href = '/auth/login';
+          window.location.href = '/#/auth/login';
         }
       });
   }
--- sprint5/UI/src/app/app-routing.module.ts
+++ sprint5/UI/src/app/app-routing.module.ts
@@ -22,7 +22,7 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes, {useHash: false, scrollPositionRestoration: 'enabled' })],
+  imports: [RouterModule.forRoot(routes, {useHash: true, scrollPositionRestoration: 'enabled' })],
   exports: [RouterModule]
 })
 export class AppRoutingModule {
