bac1507482d12917870956cacbabe3183ed71171
Roy de Kleijn <dekleijn.roy@gmail.com>
2024-06-22 09:55:23 +0200

    no message

--- deploy.php
+++ deploy.php
@@ -86,6 +86,7 @@ function deploy($source, $webDestination, $apiDestination) {
     run("/usr/bin/php /var/www/api{$apiDestination}.practicesoftwaretesting.com/artisan db:seed --force -vvvv");
     run("/usr/bin/php /var/www/api{$apiDestination}.practicesoftwaretesting.com/artisan route:cache");
     run("/usr/bin/php /var/www/api{$apiDestination}.practicesoftwaretesting.com/artisan config:clear");
+    run("/usr/bin/php /var/www/api{$apiDestination}.practicesoftwaretesting.com/artisan config:cache");
 //        run("/usr/bin/php /var/www/api{$app['api_destination']}.practicesoftwaretesting.com/artisan vendor:publish --provider \"L5Swagger\L5SwaggerServiceProvider\"");
     run("/usr/bin/php /var/www/api{$apiDestination}.practicesoftwaretesting.com/artisan l5-swagger:generate");
     run("sudo chmod -R 777 /var/www/api{$apiDestination}.practicesoftwaretesting.com/storage");
