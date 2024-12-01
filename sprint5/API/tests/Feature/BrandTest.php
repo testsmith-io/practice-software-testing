c64c49b495b4c227952096208b4ea7b83a571e01
Roy de Kleijn <dekleijn.roy@gmail.com>
2024-12-01 12:23:14 +0100

    no message

--- sprint5/API/tests/Feature/BrandTest.php
+++ sprint5/API/tests/Feature/BrandTest.php
@@ -8,7 +8,6 @@ use App\Models\Product;
 use App\Models\ProductImage;
 use App\Models\User;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
-use Illuminate\Foundation\Testing\RefreshDatabase;
 use Symfony\Component\HttpFoundation\Response as ResponseAlias;
 use Tests\TestCase;
 
