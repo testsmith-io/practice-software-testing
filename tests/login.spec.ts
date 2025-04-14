d3b24ffe4f3a8d2ee2b0042ba0d4cef8a1536b95
Roy de Kleijn <dekleijn.roy@gmail.com>
2025-04-14 11:11:09 +0200

    see if products load

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -3,7 +3,9 @@ import {test, expect} from '@playwright/test';
 test.describe('Login Feature', () => {
 
     test('Login_withValidUserCredentials_dasboardIsDisplayed', async ({page}) => {
-        await page.goto('/auth/login');
+        await page.goto('');
+
+        await page.locator('[data-test="nav-sign-in"]').click();
 
         await page.locator('[data-test="email"]').fill('customer@practicesoftwaretesting.com');
         await page.locator('[data-test="password"]').fill('welcome01');
