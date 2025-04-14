b16af3350f66856db40b1cab3f7b2cab6d36fa6e
Roy de Kleijn <dekleijn.roy@gmail.com>
2025-04-14 10:49:45 +0200

    adjusted log level

--- playwright.config.ts
+++ playwright.config.ts
@@ -28,7 +28,8 @@ export default defineConfig({
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
-    screenshot: 'on'
+    screenshot: 'on',
+    video: 'on'
   },
 
   /* Configure projects for major browsers */
