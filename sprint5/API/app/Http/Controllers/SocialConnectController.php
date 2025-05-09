7af1a83283d1c811f113e613227d020c8748f6a9
Roy de Kleijn <dekleijn.roy@gmail.com>
2025-05-09 13:43:04 +0200

    default social connect

--- sprint5/API/app/Http/Controllers/SocialConnectController.php
+++ sprint5/API/app/Http/Controllers/SocialConnectController.php
@@ -3,51 +3,53 @@
 namespace App\Http\Controllers;
 
 use App\Models\User;
+use ErrorException;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
 use Laravel\Socialite\Facades\Socialite;
 
 class SocialConnectController extends Controller
 {
     public function callbackGoogle()
     {
-        $socialUser = Socialite::driver("google")->stateless()->user();
-        $user = User::where(['uid' => $socialUser->id])->where('provider', 'google')->first();
-        if (!$user) {
-            $user = new User;
-            $user->provider = 'google';
-            $user->uid = $socialUser->id;
-            $user->email = $socialUser->email;
-            $parts = explode(' ', $socialUser->name, 2);
-            $firstName = $parts[0];
-            $lastName = $parts[1] ?? '';
-            $user->first_name = $firstName;
-            $user->last_name = $lastName;
-            $user->role = 'user';
-            $user->save();
-        }
+        try {
+            $socialUser = Socialite::driver("google")->stateless()->user();
+            $user = User::where(['uid' => $socialUser->id])->where('provider', 'google')->first();
+            if (!$user) {
+                $user = new User;
+                $user->provider = 'google';
+                $user->uid = $socialUser->id;
+                $user->email = $socialUser->email;
+                $parts = explode(' ', $socialUser->name, 2);
+                $firstName = $parts[0];
+                $lastName = $parts[1] ?? '';
+                $user->first_name = $firstName;
+                $user->last_name = $lastName;
+                $user->role = 'user';
+                $user->save();
+            }
 
-        $token = app('auth')->login($user);
+            $token = app('auth')->login($user);
 
-        // Now check user role
-        if ($token) {
+            if (isset($token)) {
+                ?>
+                <script language="javascript">
+                    if (window.opener) {
+                        window.opener.parent.location.href = "https://practicesoftwaretesting.com/auth/login;socialid=<?php echo urlencode($token); ?>";
+                    }
+                    window.self.close();
+                </script>
+                <?php
+            }
+        } catch (ErrorException $e) {
             ?>
             <script language="javascript">
                 if (window.opener) {
-                    window.opener.parent.location.href = "https://practicesoftwaretesting.com/auth/login;socialid=<?php echo urlencode($token); ?>";
+                    window.opener.parent.location.href = "https://practicesoftwaretesting.com/auth/login";
                 }
                 window.self.close();
             </script>
             <?php
         }
-        ?>
-        <script language="javascript">
-            if (window.opener) {
-                window.opener.parent.location.href = "https://practicesoftwaretesting.com/auth/login";
-            }
-            window.self.close();
-        </script>
-        <?php
     }
 
     public function getAuthUrl(Request $request)
--- sprint5/UI/src/environments/environment.ts
+++ sprint5/UI/src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  production: false,
+  production: true,
   apiUrl: 'http://localhost:8091',
   gaCode: '',
   gtmCode: ''
