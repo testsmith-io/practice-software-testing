6749ef7599ce8c50ab8e34a996e5145ef0486c91
Roy de Kleijn <dekleijn.roy@gmail.com>
2025-08-16 11:59:17 +0200

    enable blacklist

--- sprint5/API/config/jwt.php
+++ sprint5/API/config/jwt.php
@@ -217,7 +217,7 @@ return [
     |
     */
 
-    'blacklist_enabled' => env('JWT_BLACKLIST_ENABLED', false),
+    'blacklist_enabled' => env('JWT_BLACKLIST_ENABLED', true),
 
     /*
     | -------------------------------------------------------------------------
